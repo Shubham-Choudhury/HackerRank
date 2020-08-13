dfcd7a96d7ad9417d3fe7fda2d2765f2288ca5fb
Shubham Choudhury <69568389+Shubham-Choudhury@users.noreply.github.com>
2020-08-13 11:40:51 +0530

    Add files via upload

---
+++ C Programming/#11 1D Arrays in C.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+ int n;
+ scanf("%d",&n);
+ int a[n];
+ int i;
+ int s=0;
+ for(i=0;i<n;i++)
+ {
+     scanf("%d",&a[i]);
+ }   
+ for(i=0;i<n;i++)
+ {
+     s=s+a[i];
+ }
+ printf("%d",s);
+    return 0;
+}
